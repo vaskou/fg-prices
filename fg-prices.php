8e909776fe7e89b2d62e5d0281e17a5575a1abff
Vasilis Koutsopoulos <vaskou@hotmail.com>
2021-05-14 19:20:47 +0300

    -Init

---
+++ fg-prices.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @wordpress-plugin
+ * Plugin Name:       FremeditiGuitars - Prices
+ * Description:       FremeditiGuitars - Prices
+ * Version:           1.0.0
+ * Author:            Vasilis Koutsopoulos
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       fg-prices
+ * Domain Path:       /languages
+ */
+
+defined( 'ABSPATH' ) or die();
+
+define( 'FG_PRICES_VERSION', '1.0.0' );
+define( 'FG_PRICES_PLUGIN_DIR_PATH', plugin_dir_path( __FILE__ ) );
+define( 'FG_PRICES_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
+define( 'FG_PRICES_PLUGIN_DIR_NAME', basename( FG_PRICES_PLUGIN_DIR_PATH ) );
+define( 'FG_PRICES_PLUGIN_URL', plugins_url( FG_PRICES_PLUGIN_DIR_NAME ) );
+
+
+
