4a7518995d693fd1be699207fa1f9dc0cc0a4415
vaskou <vaskou@hotmail.com>
2026-09-08 14:40:32 +0300

    -WIP

--- includes/post-types-prices-hooks/class-fg-prices-fg-guitars-some-versions.php
+++ includes/post-types-prices-hooks/class-fg-prices-fg-guitars-some-versions.php
@@ -17,12 +17,14 @@ class FG_Prices_FG_Guitars_Some_Versions extends FG_Prices_Post_Types_Prices_Hoo
 		$this->multicurrency_prices_meta_key = 'fg_available_guitars_multicurrency_prices';
 
 		add_filter( 'fg_guitars_some_versions_fields', array( $this, 'post_type_fields' ) );
+		add_filter( 'fg_guitars_some_versions__featured_guitar_price', [ $this, 'get_featured_guitar_price' ], 10, 2 );
 		add_filter( 'fg_guitars_some_versions__price_range_from', [ $this, 'get_price_range_from' ], 10, 2 );
 		add_filter( 'fg_guitars_some_versions__price_range_to', [ $this, 'get_price_range_to' ], 10, 2 );
 	}
 
 	public function post_type_fields( $fields ) {
 		$price_field_names = [
+			'featured_guitar_price',
 			'price_range_from',
 			'price_range_to',
 		];
@@ -50,6 +52,20 @@ class FG_Prices_FG_Guitars_Some_Versions extends FG_Prices_Post_Types_Prices_Hoo
 		}
 
 		return $fields;
+	}
+
+	public function get_featured_guitar_price( $price, $post_id ) {
+		$old_currency = FG_Prices_Settings::instance()->get_old_currency();
+
+		$multicurrency_prices = get_post_meta( $post_id, 'fgg_some_versions_multicurrency_featured_guitar_price', true );
+
+		$multicurrency_prices = ! empty( $multicurrency_prices ) ? $multicurrency_prices : array();
+
+		if ( ! empty( $old_currency ) ) {
+			$multicurrency_prices[ $old_currency ] = $price;
+		}
+
+		return apply_filters( 'fg_prices_get_multicurrency_prices', $price, $multicurrency_prices );
 	}
 
 	public function get_price_range_from( $price, $post_id ) {
